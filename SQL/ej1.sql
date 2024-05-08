f4e492549b318178bf55a961dbf129083818dc74
Andressance <andressancenon@gmail.com>
2024-05-08 12:51:26 +0200

    Structure

---
+++ SQL/ej1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `ej1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `edad` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
